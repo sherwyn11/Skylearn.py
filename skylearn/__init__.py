7eafaa07177b59dba056fa3aaed14212a32cba70
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-12-07 12:24:41 +0530

    :hammer: fixed some issues

--- skylearn/__init__.py
+++ skylearn/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, session
+import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 app.secret_key = "HelloWorld"
 
 from skylearn import routes
